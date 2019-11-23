7130ebcd79bffbe28e2a6c80b5d50cff6b65e156
AleksSG <samuilov.aleks@gmail.com>
2019-11-23 20:12:28 +0100

    Arduino basics done. Does not dettect port

--- arduino/ard_temperature/ard_temperature.ino
+++ arduino/ard_temperature/ard_temperature.ino
@@ -1,53 +1,76 @@
 #include <ArduinoLowPower.h>
 #include <DHT.h>
-#include <SigFox.h>
+#include <SigFox.h> 
  
 #define DHTPIN 2
-#define BYTES_DATA 12
+#define LECTURES 6
+#define TIME_BETWEEN_MESURES 10000 //ms
 #define DHTTYPE DHT11
 
+#define DEBUG true
+
 DHT dht(DHTPIN, DHTTYPE);
- 
+
+class SigFoxMessage {
+  public:
+    int8_t temps[6];
+    int8_t humid[6];
+};
+
 void setup() {
-  // Configuración del monitor serie
+  //Conf serial display/monitor and time to open monitor
   Serial.begin(9600);
+  while (!Serial) {}
   dht.begin();
-  // Iniciamos módulo Sigfox
+  
+  // Init module
   if(!SigFox.begin()){
     Serial.println("Error al iniciar módulo Sigfox");
     return;
   }
- 
+  if (DEBUG) {
   // Mostramos la información del ID y de PAC
-  Serial.println("ID = " + SigFox.ID());
-  Serial.println("PAC = " + SigFox.PAC());
+    Serial.println("ID = " + SigFox.ID());
+    Serial.println("PAC = " + SigFox.PAC());
+  }
  
-  // Apagamos el módulo
+  // Stop module
   SigFox.end();
+  LowPower.attachInterruptWakeup(RTC_ALARM_WAKEUP, alarmEvent, CHANGE);
 }
  
 void loop() {
-  byte sendBytes[BYTES_DATA] = {};
-  for (int i = 0; i<BYTES_DATA/2; i++) {
-      float t = dht.readTemperature();
-      sendBytes[i*2]   = (byte) t;
-      float h = dht.readHumidity();
-      sendBytes[i*2+1] = (byte) h;
-      Serial.println("Captured lecture number "+String(i));
-      delay(5000);
-  }
-  Serial.println(String(sizeof(sendBytes)));
-  Serial.println("T1: "+ String(sendBytes[0])  + "\tH1: "+String(sendBytes[1])+
-               "\t\tT2: "+ String(sendBytes[2])  + "\tH2: "+String(sendBytes[3])+
-               "\t\tT3: "+ String(sendBytes[4])  + "\tH3: "+String(sendBytes[5])+
-               "\t\tT4: "+ String(sendBytes[6])  + "\tH4: "+String(sendBytes[7])+
-               "\t\tT5: "+ String(sendBytes[8])  + "\tH5: "+String(sendBytes[9])+
-               "\t\tT6: "+ String(sendBytes[10]) + "\tH6: "+String(sendBytes[11])
-               );
-
-    if (sizeof(sendBytes) > 12) {
-        Serial.println("Too many data, only first 12 bytes will be sent");
+    SigFoxMessage msg;
+    
+    for (int i = 0; i<LECTURES; i++) {
+        float t = dht.readTemperature();
+        msg.temps[i]  = (int8_t) t;
+        float h = dht.readHumidity();
+        msg.humid[i]  = (int8_t) h;
+        if (DEBUG) Serial.println("Captured lecture number "+String(i+1) + "\tT: "+ String(msg.temps[i])+
+                                  "\tH: "+String(msg.humid[i])+"\tBytes: "+  String(sizeof(msg.temps[i])+ sizeof(msg.humid[i])) );
+        LowPower.sleep(TIME_BETWEEN_MESURES); 
     }
-//msg.moduleTemperature = convertoFloatToInt16(temperature, 60, -60);
-//SigFox.write((uint8_t*)&msg, 12);
+    Serial.println(sizeof(msg));
+    if (sizeof(msg) > 12) Serial.println("Too many data, only first 12 bytes will be sent");
+    //Init module, 0,5 ms to wait sync.
+    /*SigFox.begin();
+    delay(500);
+
+    //Init transmit. 12 bytes of message
+    SigFox.beginPacket();
+    SigFox.write( (uint8_t*)&msg, sizeof(msg));
+    
+    // send buffer to SIGFOX network
+    if (DEBUG) {
+      switch(SigFox.endPacket()) {
+        case 0: Serial.println("Transmission ok"); break;
+        default: Serial.println("No transmission"); break;
+      }
+    }
+    SigFox.end();*/
+    Serial.flush();
+}
+void alarmEvent() {
+  if (DEBUG) Serial.println("Waking up. Starting reading...");
 }
