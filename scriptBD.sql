4126ddca82e38ed79a686ab5c293372d3b6b1f8c
catalindoja <37113386+catalindoja@users.noreply.github.com>
2019-11-23 21:01:36 +0100

    Create scriptBD.sql

---
+++ scriptBD.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE scriptlounge;
+USE scriptlounge;
+CREATE TABLE Mesura (
+	idDevice CHAR(6) NOT NULL,
+	Temps DATE NOT NULL,
+	PRIMARY KEY (idDevice)
+);
+
+CREATE TABLE Temperatura (
+	idDevice CHAR(6) NOT NULL,
+	idTemperatura INT NOT NULL,
+	valor INT NOT NULL,
+	PRIMARY KEY (idDevice,idTemperatura),
+	FOREIGN KEY (idDevice) REFERENCES Mesura(idDevice)
+);
+
+
+CREATE TABLE Humitat(
+	idDevice CHAR(6) NOT NULL,
+	idHumitat INT NOT NULL,
+	valor INT NOT NULL,
+	PRIMARY KEY (idDevice,idHumitat),
+	FOREIGN KEY (idDevice) REFERENCES Mesura(idDevice)
+);
